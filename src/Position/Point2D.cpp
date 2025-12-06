aa4b47a9efc42e06aef861beb35ef74cb66e833d
kevinmatulaa <matulakevin91@gmail.com>
2025-12-06 04:57:30 -0500

    ?

--- src/Position/Point2D.cpp
+++ src/Position/Point2D.cpp
@@ -1,7 +1,7 @@
 #include "../../include/Position/Point2D.hpp"
 
 // This is our constructor implementation. p
-Point2D::Point2D(int x, int y) : x(x), y(y) {};
+Point2D::Point2D(int x, int y) : x(x), y(y) {}
 
 // This is our function for projecting a 3D point to a 2D point.
-Point2D Point2D::Projection2D(Point3D point) {};
+Point2D Point2D::Projection2D(Point3D point) {}
